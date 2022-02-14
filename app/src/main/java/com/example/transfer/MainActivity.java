05f80b9a666aac9aa55d4bc3c2bf6064d9e6ed2e
yshubham8419 <yshubham592@gmail.com>
2022-02-14 16:54:29 +0530

    asking permissions in runtime

--- app/src/main/java/com/example/transfer/MainActivity.java
+++ app/src/main/java/com/example/transfer/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.transfer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
+    private final String[] permissions =
+            {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityResultLauncher<String> Arl=registerForActivityResult(new ActivityResultContracts.RequestPermission(),isGranted->{
+        });
+        for(String x: permissions) {
+            if (ContextCompat.checkSelfPermission(getBaseContext(), x) != PackageManager.PERMISSION_GRANTED) {
+                Arl.launch(x);
+            }
+        }
+
     }
 }
